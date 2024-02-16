d43d3fd5b19444ee60655067734d2f57450f05c2
ferdousahammed <ferdousahamedrimon2@gmail.com>
2024-02-16 19:31:22 +0600

    Coupon and Discount functionality [Added]

--- script/script.js
+++ script/script.js
@@ -18,6 +18,29 @@ for (const card of cards) {
   });
 }
 
+document.getElementById("apply-btn").addEventListener("click", () => {
+  const coupon = document
+    .getElementById("input-field")
+    .value.split(" ")
+    .join("")
+    .toUpperCase();
+  document.getElementById("input-field").value = "";
+  if (totalPrice >= 200) {
+    if (coupon === "SELL200") {
+      discountedPrice = totalPrice * 0.2;
+      setInnerTextById("discountPrice", discountedPrice.toFixed(2));
+      const updatedTotalPrice = totalPrice - discountedPrice;
+      setInnerTextById("total", updatedTotalPrice.toFixed(2));
+    } else {
+      setInnerTextById("discountPrice", 0);
+      setInnerTextById("total", totalPrice.toFixed(2));
+      alert("Please enter correct coupon code");
+    }
+  } else {
+    alert("Less that $200. Please add more products.");
+  }
+});
+
 function getInnerTextById(id) {
   return document.getElementById(id).innerText;
 }
